5d26c35cc31bed8758aca751451f4fc27a3264e1
anoukdriessen <info@anoukdriessen.nl>
2021-12-08 13:47:23 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,53 @@
-// import styling
 import './styles/Footer.css';
 import { NavLink } from "react-router-dom";
 import { getAllPages } from "../assets/data";
-
 import { MdLogout as Logout } from "react-icons/md";
 
-function NavItem({ linkTo, icon }) {
+
+function NavItem( props ) {
+    const linkTo = props.url;
+    const icon = props.icon;
+
+    // nav item to show when user is not loggedIn
+    if (!props.isLoggedIn) {
+        return <li><NavLink to={linkTo}>{icon}</NavLink></li>
+        // return <> { !props.isLoggedIn && <li><NavLink to={linkTo}>{icon}</NavLink></li> } </>
+
+    }
+    // nav item to show when user is loggedIn
+    if (props.isLoggedIn) {
+        return <li><NavLink to={linkTo}>{icon}</NavLink></li>
+        // return <> { props.isLoggedIn && <li><NavLink to={linkTo}>{icon}</NavLink></li> } </>
+    }
+
+    // nav item to show when user is moderator
+    if (props.isLoggedIn && props.isMod) {
+        return <li><NavLink to={linkTo}>{icon}</NavLink></li>
+        // return <> { props.isLoggedIn && props.isMod && <li><NavLink to={linkTo}>{icon}</NavLink></li> } </>
+    }
+
+    // normal nav item
     return <li><NavLink to={linkTo}>{icon}</NavLink></li>
-}
-function NavItemNotLoggedIn({ isLoggedIn, linkTo, icon }){
-    return <>
-        {!isLoggedIn && <li><NavLink to={linkTo}>{icon}</NavLink></li>}
-    </>
-}
-function NavItemLoggedIn( { isLoggedIn, linkTo, icon } ){
-    return <>
-        { isLoggedIn && <li><NavLink to={linkTo}>{icon}</NavLink></li> }
-    </>
-}
-function NavItemModerator( { isLoggedIn, isMod, linkTo, icon}){
-    return <>
-        { isLoggedIn && isMod && <li><NavLink to={linkTo}>{icon}</NavLink></li> }
-    </>
 }
 
 function BottomNavigation(props) {
     const pages = getAllPages();
-    // check if user is loggedin
-    let isLoggedIn = props.isLoggedIn;
-    // check if user is moderator
-    let isMod = props.isMod;
+
+    let isLoggedIn = props.isLoggedIn;     // check if user is loggedIn
+    let isMod = props.isMod;               // check if user is moderator
+
+    // url / icon / check login / check mod
+    const navItems = [
+        [ pages[7].url, pages[7].icon, isLoggedIn ],
+        [ pages[0].url, <Logout/>, isLoggedIn ],
+        [ pages[0].url, pages[0].icon,],
+        [ pages[2].url, pages[2].icon,],
+        [ pages[8].url, pages[8].icon,  isLoggedIn ],
+        [ pages[9].url, pages[9].icon,  isLoggedIn ],
+        [ pages[10].url, pages[10].icon, isLoggedIn ],
+        [ pages[12].url, pages[12].icon, isLoggedIn ],
+        [ pages[6].url, pages[6].icon,  isLoggedIn, isMod ],
+    ];
 
     return <nav>
         <div className='menu-toggle'>
@@ -41,63 +59,32 @@ function BottomNavigation(props) {
             </label>
 
             <ul>
-                <NavItemNotLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[7].url }
-                    icon = { pages[7].icon }
-                />
-                <NavItemLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[0].url }
-                    icon = { <Logout/> }
-                />
-                <NavItem
-                    linkTo = { pages[0].url }
-                    icon = { pages[0].icon }
-                />
-                <NavItem
-                    linkTo = { pages[2].url }
-                    icon = { pages[2].icon }
-                />
-                <NavItemLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[8].url }
-                    icon = { pages[8].icon }
-                />
-                <NavItemLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[9].url }
-                    icon = { pages[9].icon }
-                />
-                <NavItemLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[10].url }
-                    icon = { pages[10].icon }
-                />
-                <NavItemLoggedIn
-                    isLoggedIn = { isLoggedIn }
-                    linkTo = { pages[12].url }
-                    icon = { pages[12].icon }
-                />
-                <NavItemModerator
-                    isLoggedIn = { isLoggedIn }
-                    isMod = { isMod }
-                    linkTo = { pages[6].url }
-                    icon = { pages[6].icon }
-                />
+                { navItems.map( ( item, key ) => {
+                    return <NavItem
+                        id = { key }
+                        url = { item.slice(0,1)[0] }
+                        icon = { item.slice(1,2)[0] }
+                        isLoggedIn = { item.slice(2,3)[0] }
+                        isMod = { item.slice(3,4)[0] }
+                    />;
+                })}
             </ul>
         </div>
     </nav>
 }
 
 function Footer( { page, year, isLoggedIn, isMod } ) {
-
     return <div id="footer" className={ page.className }>
         <BottomNavigation
             isLoggedIn = { isLoggedIn }
             isMod = { isMod }
         />
-        <span id='copyright'> © { year }, design by <a href='https://www.anoukdriessen.nl'>Anouk Driessen</a></span>
+        <span id='copyright'>
+            © { year }, design by
+            <a href='https://www.anoukdriessen.nl'>
+                Anouk Driessen
+            </a>
+        </span>
     </div>;
 }
 
