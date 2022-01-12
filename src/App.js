ad4124d609ce51ae39e54e4fad890d9db70c2620
anoukdriessen <info@anoukdriessen.nl>
2022-01-12 19:43:07 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,72 +1,25 @@
 import React, {useState} from 'react';
 
-// all pages
-import Home from './pages/Home';
-import Info from './pages/Info';
-import PlantDB from './pages/PlantDB';
-import RegisterLogin from './pages/RegisterLogin';
-import Profile from './pages/Profile';
-
 // routing
 import {
     Switch,
     Route,
 } from 'react-router-dom';
 
-
-// data
-import { getAllPages } from './assets/data';
-
-const accessToken = '';
-const apiUrl = '';
+import Home from "./pages/Home";
 
 function App() {
-    const pages = getAllPages();     // lijst met alle pagina's
-    const today = new Date();        // de datum van vandaag
-
-    const [ isUserLoggedIn, setUserLoggedIn ] = useState(false);
-    const [ isMod, setUserIsMod ] = useState(false);
-
-
 
     return (
             <Switch>
-                <Route exact path={pages[0].url}>
+                <Route exact path={'/'}>
                     <Home
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
+                        title = "Mengelmoestuintjes"
+                        loggedIn = {true}
+                        user = "vivalanouk"
                     />
                 </Route>
-                <Route path={pages[1].url}>
-                    <Info
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
-                    />
-                </Route>
-                <Route path={pages[2].url}>
-                    <PlantDB
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
-                    />
-                </Route>
-                <Route path={pages[3].url}>
-                    <RegisterLogin
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
-                    />
-                </Route>
-                <Route path={pages[4].url}>
-                    <Profile
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
-                    />
-                </Route>
-                <Route path={pages[5].url}>
-                    <Profile
-                        isLoggedIn = { isUserLoggedIn }
-                        isMod = { isMod }
-                    />
-                </Route>
+
             </Switch>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,109 +1,135 @@
-import React, {useEffect, useState} from 'react';
-import '../components/styles/pages/Info.css';
-
-import Container from "../components/main/Container";
 
 import {
-    getAllPages
-} from "../assets/data";
+    FiLogIn as Login,
+    FiUser as User
+} from "react-icons/fi"
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import Quote from "../components/Quote";
-import Cards from "../components/Cards";
-import CallToAction from "../components/CallToAction";
+function Content( props ) {
 
-const pages = getAllPages();
-const homepage = pages[0];
+}
 
-export function HomeContent( props ) {
-    const [qotd, setQuote] = useState();
-    const [error, setError] = useState();
-
-    const randomQuote = "http://localhost:8080/quotes/random";
-
-    useEffect(() => {
-        axios(randomQuote)
-            .then((response) => {
-                // console.log(response.data);
-                setQuote(response.data);
-            })
-            .catch((er) => {
-                console.error("Error occurred fetching random quote: ", er);
-                setError(er);
-            });
-    }, []);
-
-    if (error) homepage.content.quote = false;
-    homepage.content.quote = {qotd};
-
-    homepage.content.missions = {
-        organizing: {
-            title: 'organiseren',
-            description:
-                'Mengelmoestuintjes helpt je het overzicht te houden in je (moes)tuin,' +
-                ' jij bepaalt de grootte, jij bepaalt de naam, jij bepaalt wat er geplant is' +
-                ' en wij houden je op de hoogte en motiveren je taken en werkzaamheden uit te' +
-                ' voeren op basis van de planten die je hebt geplant of het seizoen dat het' +
-                ' momenteel is. Je leert het overzicht te houden en prioriteiten te maken voor' +
-                ' wat voor jou belangrijk is.',
-            background: 'organized',
-        },
-        sharing: {
-            title: 'delen',
-            description:
-                'Mengelmoestuintjes geeft je de ruimte te delen wat jij wilt!' +
-                ' Deel je tuin, oogst of mijlpaal, stel vragen aan je medetuinierders' +
-                ' of laat een notitie achter voor toekomst jij. Samen wordt het leuker!' +
-                ' & daar helpen wij je graag mee ',
-            background: 'sharing',
-        },
-        learning: {
-            title: 'leren',
-            description:
-                'Mengelmoestuintjes begrijpt dat we allemaal (nog) geen expert zijn' +
-                ' en we altijd nog wel iets kunnen leren, je kunt leren van onze' +
-                ' geselecteerde experts of je kunt leren van elkaar. Voor alle leden bieden' +
-                ' wij de mogelijkheid meer te leren over de basisbeginselen van het tuinieren' +
-                ' en de moestuin. & samen groeien we verder',
-            background: 'learning',
-        },
+function UpperLink( props ) {
+    if (props.link === '/registreren-login') {
+        return <Link className='top-right' to={props.link}>
+            <Login/>
+        </Link>
+    } else {
+        return <Link className='top-right' to={props.link}>
+            <User/>
+        </Link>
     }
-
-    // card content
-    const missions = [
-        props.content.missions.organizing,
-        props.content.missions.sharing,
-        props.content.missions.learning
-    ]
-
-
-    return <>
-        <main>
-            <Quote
-                quote={props.content.quote.qotd}
-                styling='quote'
-            />
-
-            <Cards
-                items={missions}
-            />
-
-            <CallToAction
-                type={0}
-                title={'lees meer over onze missie'}
-                linkTo={pages[1].url}
-            />
-        </main>
-    </>
+}
+function Missions( props ) {
+    if (props.missions) {
+        // console.log(props.missions);
+        return <div>
+            {
+                props.missions.map((m) => {
+                    return <div key={m.title}>
+                        <h3 className='uppercase'>{m.title}</h3>
+                        <p>{m.description}</p>
+                    </div>
+                })
+            }
+        </div>
+    }
+    return null
+}
+function Quote( props ) {
+    if (props.quote) {
+        return <div id='quote'>
+            <span className='text'>{props.quote.text}</span>
+            <br/>
+            <span>{props.quote.author}</span>
+        </div>
+    }
+    return <div id='quote'>
+        <span className='text'>hello world</span>
+        <br/>
+        <span>every programmer at the start</span>
+    </div>
+}
+function CallToAction(props) {
+    if (props.link) {
+        return <Link to={props.link}>
+            {props.action}
+        </Link>
+    }
+    return null
 }
 
 function Home( props ) {
-  return (
-      <Container
-        page = { homepage }
-        isLoggedIn = { props.isLoggedIn }
-        isMod = { props.isMod }
-      />
-  );
+    const [quote, setQuote] = useState(null);
+
+    useEffect( () => {
+            axios("https://localhost:8443/api/quotes/random")
+                .then((response) => {
+                    // console.log(response.data);
+                    setQuote(response.data);
+                })
+                .catch((er) => { console.error("Error occurred fetching random quote: ", er); });
+    }, []);
+
+    let link = '/registreren-login';
+    if (props.loggedIn && props.user) link = '/profile/' + props.user;
+
+    let content = {
+        missions: {
+            organize: {
+                title: 'organiseren',
+                description:
+                    'Mengelmoestuintjes helpt je het overzicht te houden in je (moes)tuin,' +
+                    ' jij bepaalt de grootte, jij bepaalt de naam, jij bepaalt wat er geplant is' +
+                    ' en wij houden je op de hoogte en motiveren je taken en werkzaamheden uit te' +
+                    ' voeren op basis van de planten die je hebt geplant of het seizoen dat het' +
+                    ' momenteel is. Je leert het overzicht te houden en prioriteiten te maken voor' +
+                    ' wat voor jou belangrijk is.',
+                background: 'organized',
+            },
+            share: {
+                title: 'delen',
+                description:
+                    'Mengelmoestuintjes geeft je de ruimte te delen wat jij wilt!' +
+                    ' Deel je tuin, oogst of mijlpaal, stel vragen aan je medetuinierders' +
+                    ' of laat een notitie achter voor toekomst jij. Samen wordt het leuker!' +
+                    ' & daar helpen wij je graag mee ',
+                background: 'sharing',
+            },
+            learn: {
+                title: 'leren',
+                description:
+                    'Mengelmoestuintjes begrijpt dat we allemaal (nog) geen expert zijn' +
+                    ' en we altijd nog wel iets kunnen leren, je kunt leren van onze' +
+                    ' geselecteerde experts of je kunt leren van elkaar. Voor alle leden bieden' +
+                    ' wij de mogelijkheid meer te leren over de basisbeginselen van het tuinieren' +
+                    ' en de moestuin. & samen groeien we verder',
+                background: 'learning',
+            }
+        }
+    }
+    let missions = [ content.missions.organize, content.missions.share, content.missions.learn ]
+
+    return <>
+        <div className='page-content'>
+            <h1 className='title'>{props.title}</h1>
+
+            <UpperLink link={link}/>
+
+            <Quote quote={quote}/>
+
+            <main className='centered'>
+                <h2>Onze missie</h2>
+                <Missions missions={missions}/>
+            </main>
+        </div>
+
+        <footer className='footer'>
+            <CallToAction link={'/info'} action={'maak een tuintje'}/>
+        </footer>
+
+    </>
 }
 
 export default Home;
