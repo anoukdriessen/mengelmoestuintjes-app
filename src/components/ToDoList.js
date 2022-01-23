c22a1eb821c49f330407cc3671d48d63ab539c22
Anouk Driessen <info@anoukdriessen.nl>
2022-01-23 16:05:52 +0100

    Create ToDoList.js

---
+++ src/components/ToDoList.js
@@ -0,0 +1,15 @@
+function ToDoList({thisUser}) {
+    let task = {
+        'owner': thisUser.username,
+        'type': 'TODO',
+        'title': 'taak',
+        'description': 'beschrijving',
+        'done': false,
+        'created': '2022-01-23',
+        'deadline': '2022-01-25'
+    }
+
+    return
+}
+
+export default ToDoList;
